99f023fa81e27fdd4bbe1826ca61c7d4d964f161
Zester Quinn Albano <zester.albano@lanexus.com>
2022-02-13 21:25:10 +0800

    remove ;

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
         <title>#KeZesIT</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   )
 }
